604053db97ca758950b78d3742ee43c4df3f7283
Evan Krone <evankrone1@gmail.com>
2019-08-22 23:42:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(sentence)
+\def reverse_each_word(sentence)
 sentence = ("#{sentence}")
 sentence_as_array = sentence.split (/ /)
 reversed_array = sentence_as_array.collect do |sentence_as_array| "#{sentence_as_array.reverse}"
